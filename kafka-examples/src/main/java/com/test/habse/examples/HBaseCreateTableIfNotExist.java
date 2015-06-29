ef3b3c4a758b9b1d536a7b37cf47778a8a90ffdc
EugeneYushin <evgenij.ushin@gmail.com>
2015-06-29 07:39:14 -0700

    Addition of col. family in HBase examples for table creation

--- kafka-examples/src/main/java/com/test/habse/examples/HBaseCreateTableIfNotExist.java
+++ kafka-examples/src/main/java/com/test/habse/examples/HBaseCreateTableIfNotExist.java
@@ -3,11 +3,13 @@ package com.test.habse.examples;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
 
+import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.ConnectionFactory;
 
+import org.apache.hadoop.hbase.exceptions.DeserializationException;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
@@ -16,9 +18,10 @@ import java.io.IOException;
 public class HBaseCreateTableIfNotExist {
     private static final Logger log = Logger.getLogger(HBaseCreateTableIfNotExist.class);
 
-    public static void main(String[] args) throws ConfigurationException, IOException {
+    public static void main(String[] args) throws ConfigurationException, IOException, DeserializationException {
         PropertiesConfiguration conf = new PropertiesConfiguration("hbase.properties");
         TableName tableName = TableName.valueOf(conf.getString("table.name"));
+        String colFamily = conf.getString("column.family");
 
         log.setLevel(Level.DEBUG);
         log.debug("Create HBase connection");
@@ -27,7 +30,13 @@ public class HBaseCreateTableIfNotExist {
         if (!conn.getAdmin().tableExists(tableName)) {
             log.debug(String.format("Table [%s] doesn't exist. Creating table...", tableName.getNameAsString()));
             HTableDescriptor desc = new HTableDescriptor(tableName);
+            HColumnDescriptor family = new HColumnDescriptor(colFamily);
+
+            desc.addFamily(family);
             conn.getAdmin().createTable(desc);
+
+            log.debug(String.format("Table [%s] with column family [%s] created!", tableName.getNameAsString(),
+                    colFamily));
         } else {
             log.debug(String.format("Table [%s] already exist. Exit", tableName.getNameAsString()));
             System.exit(0);
