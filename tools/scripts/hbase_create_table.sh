7f4f3a2c0cfcb9237f01dc10827364df4975b1b7
EugeneYushin <evgenij.ushin@gmail.com>
2015-06-30 07:22:54 -0700

    HBase tools: Read args from command line

--- tools/scripts/hbase_create_table.sh
+++ tools/scripts/hbase_create_table.sh
@@ -2,16 +2,25 @@
 
 # Use 'http://hbase.apache.org/book.html#_running_the_shell_in_non_interactive_mode' for reference
 
-CONFIG_DIR='../../kafka-examples/src/main/resources'
-CONFIG_FILE=${CONFIG_DIR}/'hbase.properties'
-
-TBL_NAME=$(grep "table.name" $CONFIG_FILE | cut -d'=' -f 2 | tr -d '[[:space:]]')
-COL_FAMILY=$(grep "column.family" $CONFIG_FILE | cut -d'=' -f 2 | tr -d '[[:space:]]')
-
+#CONFIG_DIR='../../kafka-examples/src/main/resources'
+#CONFIG_FILE=${CONFIG_DIR}/'hbase.properties'
+#
+#TBL_NAME=$(grep "table.name" $CONFIG_FILE | cut -d'=' -f 2 | tr -d '[[:space:]]')
+#COL_FAMILY=$(grep "column.family" $CONFIG_FILE | cut -d'=' -f 2 | tr -d '[[:space:]]')
+#
 # Alternative way, but it's not permitted to use dots in bash vars
-# CONF=${CONF:=$CONFIG_FILE}
-# if [ -f "${CONF}" ]; then . ${CONF}; fi
-# echo ${table_name}
+#CONF=${CONF:=$CONFIG_FILE}
+#if [ -f "${CONF}" ]; then . ${CONF}; fi
+#echo ${table_name}
+
+if ! (( $# == 2 )); then
+    echo "ERROR: Wrong number of parameters." 1>&2
+    echo "Usage: hbase_create_table.sh [TBL_NAME] [COL_FAMILY]" 1>&2
+    exit 1
+fi
+
+TBL_NAME=$1
+COL_FAMILY=$2
 
 echo "Creating table [$TBL_NAME], family [$COL_FAMILY]"
 
