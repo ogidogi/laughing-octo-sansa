bed0fc307ae3327a67c14bb8587f93725cc41023
EugeneYushin <evgenij.ushin@gmail.com>
2015-06-30 01:17:40 -0700

    Add HBase table creation (Shell version)

---
+++ tools/bin/hbase_create_table.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Use 'http://hbase.apache.org/book.html#_running_the_shell_in_non_interactive_mode' for reference
+
+CONFIG_DIR='../../kafka-examples/src/main/resources'
+CONFIG_FILE=${CONFIG_DIR}/'hbase.properties'
+
+TBL_NAME=$(grep "table.name" $CONFIG_FILE | cut -d'=' -f 2 | tr -d '[[:space:]]')
+COL_FAMILY=$(grep "column.family" $CONFIG_FILE | cut -d'=' -f 2 | tr -d '[[:space:]]')
+
+echo "Creating table [$TBL_NAME], family [$COL_FAMILY]"
+
+set +o errexit
+hbase shell -n <<EOF
+disable '$TBL_NAME'
+drop '$TBL_NAME'
+create '$TBL_NAME', '$COL_FAMILY'
+list '$TBL_NAME'
+EOF
+set -o errexit
+
+echo "Exit"
+exit 0
