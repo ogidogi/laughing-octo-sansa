d9f97241b8dcddd081b888b67edad33b39a00ca9
EugeneYushin <evgenij.ushin@gmail.com>
2015-07-02 03:23:00 -0700

    Correct package name of hbase.examples

--- kafka-examples/src/main/java/com/test/hbase/examples/HBaseCreateTableIfNotExist.java
+++ kafka-examples/src/main/java/com/test/hbase/examples/HBaseCreateTableIfNotExist.java
@@ -1,4 +1,4 @@
-package com.test.habse.examples;
+package com.test.hbase.examples;
 
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
