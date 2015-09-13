99d71f47dbd1c8c2bafd833406965f44e10014fc
EugeneYushin <evgenij.ushin@gmail.com>
2015-09-13 06:50:12 -0700

    Add Kafka Producer generator

---
+++ bidding-support-engine/src/main/java/org/bidsup/engine/kafka/producers/KafkaRtbProducer.java
@@ -0,0 +1,63 @@
+package org.bidsup.engine.kafka.producers;
+
+import kafka.javaapi.producer.Producer;
+import kafka.producer.KeyedMessage;
+import kafka.producer.ProducerConfig;
+import org.apache.commons.configuration.CompositeConfiguration;
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import twitter4j.internal.logging.Logger;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.Random;
+
+public class KafkaRtbProducer {
+    static Logger log = Logger.getLogger(KafkaRtbProducer.class);
+
+    public static void main(String[] args) throws ConfigurationException, InterruptedException {
+        Random rnd = new Random();
+
+        CompositeConfiguration conf = new CompositeConfiguration();
+        conf.addConfiguration(new PropertiesConfiguration("kafka.properties"));
+
+        Properties props = new Properties();
+        props.put("metadata.broker.list", "quickstart.cloudera:9092");
+        props.put("serializer.class", "kafka.serializer.StringEncoder");
+//        props.put("partitioner.class", "example.producer.SimplePartitioner");
+        props.put("request.required.acks", "1");
+
+        ProducerConfig config = new ProducerConfig(props);
+        Producer<String, String> producer = new Producer<String, String>(config);
+
+        try (BufferedReader br = new BufferedReader(new FileReader("/media/sf_Download/ipinyou/window/clk.20130607.txt")))
+        {
+
+            String currentLine;
+            int numLines = rnd.nextInt(10) + 1;
+
+            log.info(String.format("Read %d lines...", numLines));
+            while ((currentLine = br.readLine()) != null) {
+                if (numLines > 0) {
+                    KeyedMessage<String, String> data = new KeyedMessage<String, String>("test_topic", currentLine);
+                    producer.send(data);
+                    numLines--;
+                } else {
+                    KeyedMessage<String, String> data = new KeyedMessage<String, String>("test_topic", currentLine);
+                    producer.send(data);
+                    numLines = rnd.nextInt(10) + 1;
+                    Thread.sleep(3000);
+                    log.info(String.format("Read %d lines...", numLines));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            producer.close();
+        }
+
+        log.info("EOF");
+        producer.close();
+    }
+}
