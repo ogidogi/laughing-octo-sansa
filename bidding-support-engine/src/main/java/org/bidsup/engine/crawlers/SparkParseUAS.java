8a3db8a3e813e0e51f3c44060b83db7d0a000546
ogidogi <kirills2006@gmail.com>
2015-09-15 12:21:51 -0700

    Facebook crawler small fixies2

--- bidding-support-engine/src/main/java/org/bidsup/engine/crawlers/SparkParseUAS.java
+++ bidding-support-engine/src/main/java/org/bidsup/engine/crawlers/SparkParseUAS.java
@@ -5,9 +5,13 @@ import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.DataFrame;
 import org.apache.spark.sql.SQLContext;
 import org.apache.spark.sql.types.DataTypes;
-import org.bidsup.engine.spark.sql.udf.ParseUserAgentString;
 
-import static org.apache.spark.sql.functions.callUDF;
+import eu.bitwalker.useragentutils.UserAgent;
+import scala.runtime.AbstractFunction1;
+
+import static org.apache.spark.sql.functions.*;
+
+import java.io.Serializable;
 
 public class SparkParseUAS {
 
@@ -18,21 +22,42 @@ public class SparkParseUAS {
         SQLContext sqlContext = new SQLContext(jsc);
 
         DataFrame df = sqlContext.read().format("com.databricks.spark.csv").option("header", "false").option("delimiter", "\t")
-<<<<<<< Updated upstream
                 .load("/mnt/data/workspace/laughing-octo-sansa/data/clk.20130606.txt");
 
         df.withColumn("browser", callUDF(new ParseUserAgentString("browser"), DataTypes.StringType, df.col("C4")))
                 .withColumn("browserVersion", callUDF(new ParseUserAgentString("browserVersion"), DataTypes.StringType, df.col("C4")))
                 .withColumn("id", callUDF(new ParseUserAgentString("id"), DataTypes.StringType, df.col("C4")))
                 .withColumn("operatingSystem", callUDF(new ParseUserAgentString("operatingSystem"), DataTypes.StringType, df.col("C4")))
-=======
-                .load("/mnt/data/workspace/laughing-octo-sansa/data/bid.20130606.txt");
-        df.withColumn("browser", callUDF(new ParseUserAgentString("browser"), DataTypes.StringType, df.col("C3")))
-                .withColumn("browserVersion", callUDF(new ParseUserAgentString("browserVersion"), DataTypes.StringType, df.col("C3")))
-                .withColumn("id", callUDF(new ParseUserAgentString("id"), DataTypes.StringType, df.col("C3")))
-                .withColumn("operatingSystem", callUDF(new ParseUserAgentString("operatingSystem"), DataTypes.StringType, df.col("C3")))
->>>>>>> Stashed changes
-                .write().format("com.databricks.spark.csv").option("header", "false").option("delimiter", "\t")
+
+        .write().format("com.databricks.spark.csv").option("header", "false").option("delimiter", "\t")
                 .save("/mnt/data/workspace/laughing-octo-sansa/data/bid.20130606_parsed.txt");
+    }
+}
+
+class ParseUserAgentString extends AbstractFunction1<String, String>implements Serializable {
+
+    private static final long serialVersionUID = -1474997808699658439L;
+
+    private final String userAgentComponent;
+
+    public ParseUserAgentString(String userAgentComponent) {
+        this.userAgentComponent = userAgentComponent;
+    }
+
+    @Override
+    public String apply(String value) {
+        final UserAgent userAgent = UserAgent.parseUserAgentString(value);
+        switch (userAgentComponent) {
+        case "browser":
+            return String.valueOf(userAgent.getBrowser());
+        case "browserVersion":
+            return String.valueOf(userAgent.getBrowserVersion());
+        case "id":
+            return String.valueOf(userAgent.getId());
+        case "operatingSystem":
+            return String.valueOf(userAgent.getOperatingSystem());
+        default:
+            return null;
+        }
     }
 }
