32e1958fdb86106e1978f629df7b63babf60aa1f
ogidogi <kirills2006@gmail.com>
2015-10-31 09:50:00 -0700

    Add threads and reading from multiple files for Kafka Producer

--- marketing-support-engine/src/main/java/org/marksup/engine/kafka/producers/KafkaRtbProducer.java
+++ marketing-support-engine/src/main/java/org/marksup/engine/kafka/producers/KafkaRtbProducer.java
@@ -12,13 +12,22 @@ import org.apache.log4j.Logger;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Properties;
 import java.util.Random;
 
+import static java.lang.Thread.currentThread;
+import static java.lang.Thread.sleep;
+
 public class KafkaRtbProducer {
     private static final Logger log = Logger.getLogger(KafkaRtbProducer.class);
+    private static final int sleepMsInterval = 4000;
+    private static Path dirPath = Paths.get("/media/sf_Download/data/mors/new");
 
-    public static void main(String[] args) throws ConfigurationException, InterruptedException {
+
+    public static void main(String[] args) throws ConfigurationException, InterruptedException, IOException {
         log.setLevel(Level.DEBUG);
         Random rnd = new Random();
 
@@ -32,33 +41,46 @@ public class KafkaRtbProducer {
         props.put("request.required.acks", "1");
 
         ProducerConfig config = new ProducerConfig(props);
-        Producer<String, String> producer = new Producer<String, String>(config);
+//        Producer<String, String> producer = new Producer<String, String>(config);
 
-        try (BufferedReader br = new BufferedReader(new FileReader("/media/sf_Download/data/mors/new/site-click.20130606-aa.txt"))) {
+        Files.walk(dirPath).forEach(file -> {
+            if (Files.isRegularFile(file)) {
+                String fileName = file.getFileName().toString();
+                log.info(String.format("[%s] Processing %s", currentThread().getName(), fileName));
 
-            String currentLine;
-            int numLines = rnd.nextInt(10) + 1;
+                Thread fileThread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Producer<String, String> producer = new Producer<String, String>(config);
+                        try (BufferedReader br = new BufferedReader(new FileReader(file.toFile()))) {
+                            String currentLine;
+                            int numLines = rnd.nextInt(10) + 1;
 
-            log.info(String.format("Read %d lines...", numLines));
-            while ((currentLine = br.readLine()) != null) {
-                if (numLines > 0) {
-                    KeyedMessage<String, String> data = new KeyedMessage<String, String>("test_topic", currentLine);
-                    producer.send(data);
-                    numLines--;
-                } else {
-                    KeyedMessage<String, String> data = new KeyedMessage<String, String>("test_topic", currentLine);
-                    producer.send(data);
-                    numLines = rnd.nextInt(10) + 1;
-                    Thread.sleep(3000);
-                    log.info(String.format("Read %d lines...", numLines));
-                }
+                            log.info(String.format("[%s]:[%s] Read %d lines...", currentThread().getName(), fileName, numLines));
+                            while ((currentLine = br.readLine()) != null) {
+                                if (numLines > 0) {
+                                    KeyedMessage<String, String> data = new KeyedMessage<String, String>("test_topic", currentLine);
+                                    producer.send(data);
+                                    numLines--;
+                                } else {
+                                    KeyedMessage<String, String> data = new KeyedMessage<String, String>("test_topic", currentLine);
+                                    producer.send(data);
+                                    numLines = rnd.nextInt(10) + 1;
+                                    sleep(sleepMsInterval);
+                                    log.info(String.format("[%s]:[%s] Read %d lines...", currentThread().getName(), fileName, numLines));
+                                }
+                            }
+                        } catch (IOException | InterruptedException e) {
+                            e.printStackTrace();
+                            producer.close();
+                        }
+
+                        log.info(String.format("[%s]:[%s] end-of-file", currentThread().getName(), fileName));
+                        producer.close();
+                    }
+                });
+                fileThread.start();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-            producer.close();
-        }
-
-        log.info("EOF");
-        producer.close();
+        });
     }
 }
